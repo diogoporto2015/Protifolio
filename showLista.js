8e66b82f96bc70989157363dbb81f3d2907f21ec
DiogoPS <diogoporto@unigranrio.br>
2023-05-20 13:24:18 -0300

    atu
    atua

--- showLista.js
+++
@@ -1,9 +0,0 @@
-function showLista(lista, buttom = "all") {
-    lista.forEach((item) => {
-        item.classList.remove('ativo');
-    });
-    if (buttom == 'design') {
-        lista[5].classList.add('ativo');
-        lista[7].classList.add('ativo');
-    }
-}
